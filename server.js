8fafa8af9995f548507b29d2d4546083c2e60eff
Drkeck <k3ckl3y.a@gmail.com>
2020-05-26 14:20:05 -0700

    started express server sections

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//api's
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    });
+});
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+// server listen
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
