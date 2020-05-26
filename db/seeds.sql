6a05d51b7c81d5e04b3577c5e31459523ee33a19
Drkeck <k3ckl3y.a@gmail.com>
2020-05-26 13:43:41 -0700

    added basic sql functionality

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES
+    ('Ronald', 'firbank', 1),
+    ('Virginia', 'woolf', 1),
+    ('piers', 'gaveston', 0),
+    ('Charles', 'LeRoi', 1),
+    ('Katherine', 'Mansfield', 1),
+    ('Dora', 'Carrington', 0),
+    ('Edward', 'Bellamy', 0),
+    ('Montague', 'Summers', 1),
+    ('Octavia', 'Butler', 1),
+    ('unica', 'zurn', 1);
